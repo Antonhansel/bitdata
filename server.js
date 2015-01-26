cc862416e90534045a299e7727e026a849af3a44
antonhansel <antoninribeaud@gmail.com>
2015-01-26 12:44:07 +0100

    fetching data

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 // server.js
-var config = require('./lib/config/config.json');
+var config 		 = require('./lib/config/config.json');
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose').connect(process.env.MONGOURL);
-
+var express  	 = require('express');
+var app      	 = express();
+var port     	 = process.env.PORT || 8080;
 var morgan       = require('morgan');
 var bodyParser   = require('body-parser');
 var path 		 = require('path'); 
-var crawler 	 = require('./lib/crawler.js');
-// Setting git data refresh
-
+// Setting bitcoin data refresh
+var ticker;
+var stats;
+var crawler 	 = require('./lib/app/crawler.js');
+crawler.refreshData();
+setInterval(crawler.refreshData(), 60000*10);
 // configuration ===============================================================
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser()); // get information from html forms
-
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
 app.use(express.static(path.join(__dirname, 'public'))); // to get local files
 
 // routes ======================================================================
-require('./lib/app/routes.js')(app, mongoose); // load our routes and pass in our app
+require('./lib/app/routes.js')(app); // load our routes and pass in our app
 
 // launch ======================================================================
 app.listen(port);
